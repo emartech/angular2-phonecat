f6b29c262cca960a49f392315d67d9b12742f348
blacksonic <soos.gabor86@gmail.com>
2016-10-02 20:43:16 +0200

    Updates

--- style.bundle.js
+++ style.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1],{236:function(n,c){},237:function(n,c){},238:function(n,c){},385:function(n,c,t){"use strict";t(236),t(237),t(238)}},[385]);
+webpackJsonp([1],{236:function(n,c){},237:function(n,c){},238:function(n,c){},385:function(n,c,o){o(236),o(237),o(238)}},[385]);
 //# sourceMappingURL=style.map
