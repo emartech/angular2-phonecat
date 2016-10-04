6fbb4b87c8f292514751b4443789f60d8c2d70fd
blacksonic <soos.gabor86@gmail.com>
2016-10-04 21:20:33 +0200

    cleanup dev dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,5 @@
 'use strict';
-let path = require('path');
 
-let npmLifecycleEvent = process.env.npm_lifecycle_event || '';
-let isSingleRun = npmLifecycleEvent === 'test';
-
-module.exports = function(config) {
-  config.set({
-    basePath: '',
-
-    frameworks: ['jasmine'],
-
-    files: [
-      { pattern: 'src/app/test.ts' }
-    ],
-
-    preprocessors: {
-      'src/app/test.ts': ['webpack', 'sourcemap']
-    },
-
-    reporters: ['dots'],
-
-    webpack: require('./webpack.config'),
-    webpackServer: { noInfo: true },
-
-    port: 9876,
-
-    colors: true,
-
-    logLevel: config.LOG_INFO,
-
-    browsers: ['PhantomJS'],
-
-    autoWatch: !isSingleRun,
-    singleRun: isSingleRun
-  });
-};
+module.exports = require('angular2-config-generator').karma(
+  require('./webpack.config')
+);
