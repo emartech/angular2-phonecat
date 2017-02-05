d7ee58a5481bcd35c9eb4f0c10c0cc557221f0f9
blacksonic <soos.gabor86@gmail.com>
2017-02-05 09:39:06 +0100

    add env var

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import '../style/app.css';
 
 import { MainModule } from './modules/main.module';
 
+if (ENV === 'production') {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(MainModule);
